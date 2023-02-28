9fff8de4e472add9fad714cec5f586af13cb6064
haluktutuncu <112336855+haluktutuncu@users.noreply.github.com>
2023-02-28 17:15:28 +0300

    Big-fix v.1

--- script.js
+++ script.js
@@ -1,34 +1,72 @@
-let textbox = document.getElementById("textbox");
-let addBtn = document.getElementById("add-btn");
-let checkBtn = document.getElementById("tick-btn")
-let main = document.getElementById("main");
-let toCopy = document.querySelector(".do-element");
-let child = toCopy.querySelector("#do")
-let deleteBtns = document.getElementsByClassName("delete")
-let items = document.querySelectorAll(".do-element")
-let deleter = document.getElementById("delete")
+// Create a "close" button and append it to each list item
+let myNodelist = document.getElementsByTagName("LI");
 
-let isFirstItem = true;
+for (let i = 0; i < myNodelist.length; i++) {
+    let span = document.createElement("SPAN");
+    let txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    myNodelist[i].appendChild(span);
+}
+// Click on a close button to hide the current list item
+let close = document.getElementsByClassName("close");
+console.log(typeof close);
 
-addBtn.addEventListener("click", () => {
-    if (textbox.value == "") {
-        alert("Enter a task");
-    } else {
-        child.innerHTML = textbox.value;
-        toCopy.style.display = "flex";
-
-        let paragraf = toCopy.cloneNode(true);
-        main.insertBefore(paragraf, main.childNodes[0])
-        textbox.value = ""
-
-        if (isFirstItem) {
-            main.removeChild(main.childNodes[2]);
-            isFirstItem = false;
-        }
-
-
-
+for (let index = 0; index < close.length; index++) {
+    close[index].onclick = function () {
+        let div = this.parentElement;
+        div.style.display = "none"
     }
-})
 
+}
+
+// Add a "checked" symbol when clicking on a list item
+let list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    // Tıklanan öğenin bir <li> öğesi olup olmadığını kontrol et
+    if (ev.target.tagName === 'LI') {
+        // Tıklanan öğenin checked sınıfını ekleyin veya kaldırın
+        ev.target.classList.toggle('checked');
+    }
+});
+
+function addElement() {
+
+    let li = document.createElement("li");
+    let inputValue = document.querySelector("#input").value;
+    let t = document.createTextNode(inputValue);
+
+    li.appendChild(t);
+
+    if (inputValue === "") { alert("You must write something") }
+    else { document.getElementById("myList").appendChild(li) }
+    document.getElementById("input").value = "";
+
+    var span = document.createElement("SPAN");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "close";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
 
